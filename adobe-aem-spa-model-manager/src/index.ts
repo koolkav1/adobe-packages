99275ff9942091fbea401974f705a6873690c778
Kanwarjeet Khalsa <kenny.khalsa@gmail.com>
2024-07-13 13:14:44 +0100

    adding exports

--- adobe-aem-spa-model-manager/src/index.ts
+++ adobe-aem-spa-model-manager/src/index.ts
@@ -1,1 +1,18 @@
-export * from './lib/adobe-aem-spa-model-manager/adobe-aem-spa-model-manager.component';
+import { FormGroupName } from '@angular/forms';
+
+export * from './lib/common/constants';
+export * from './lib/common/deep-clone';
+export * from './lib/common/domain-token';
+export * from './lib/common/internal-contants';
+export * from './lib/common/model-manager.interface';
+export * from './lib/common/model.interface';
+export * from './lib/common/path-utils.interface';
+
+export * from './lib/services/editor-client.service';
+export * from './lib/services/model-client.service';
+export * from './lib/services/model-manager.service'; 
+export * from './lib/services/model-router.service';
+export * from './lib/services/model-store.service';
+
+export * from './lib/utils/authoring.service';
+export * from './lib/utils/path.service';
